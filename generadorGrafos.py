8c35172f067e821caaa6bdf14313ac2dd4384a4d
Federico Hidalgo <fhidalgom1500@alumno.ipn.mx>
2024-10-21 11:15:20 -0600

    Se agrega la clase Nodo

--- generadorGrafos.py
+++ generadorGrafos.py
@@ -1,1 +1,21 @@
-#
+class Nodo:
+    def __init__(self, valor):
+        """
+        Constructor de la clase Nodo
+        """
+        self.valor = valor
+        self.attr = {
+            """
+            Diccionario en construcción
+            """
+        }
+
+    def getNodo(self):
+        print("El nodo es:")
+        print(self.valor)
+
+nodo1 = Nodo(5)
+nodo2 = Nodo(12)
+
+nodo1.getNodo()
+nodo2.getNodo()
