8128809fa5d180b3cf14d67a0aa642c0049ef8ac
Federico Hidalgo <fhidalgom1500@alumno.ipn.mx>
2024-10-21 16:14:43 -0600

    se crea la biblioteca generadora de Nodos

--- generadorGrafos.py
+++ generadorGrafos.py
@@ -1,14 +1,55 @@
 class Nodo:
-    def __init__(self, valor):
+    """
+    Clase generadora de Nodos
+    """
+    def __init__(self, id):
         """
-        Constructor de la clase Nodo
+        Constructor
         """
-        self.valor = valor
+        self.id = id      #Identificador del nodo
         self.attr = {
             """
             Diccionario en construcción
             """
         }
 
+    def __str__(self):
+        """
+        Convertir nodo en string
+        """
+        return str(self.id)
+
     def getNodo(self):
-        print(self.valor)
+        """
+        Método para imprimir el identificador del nodo
+        """
+        print(self.id)
+
+class Arista:
+    """
+    Clase generadora de Aristas
+    """
+    def __init__(self,nodo0, nodo1, id):
+        """
+        Constructor
+        """
+        self.nodo0 = nodo0  #Nodo de origen
+        self.nodo1 = nodo1  #Nodo de destino  
+        self.id =  id       #Identificador de la arista
+        self.attr = {
+            """
+            Diccionario en construcción
+            """
+        }
+
+    def getArista(self):
+        """
+        Método para imprimir los valores de la Arista
+        """     
+        print(self.nodo0, self.nodo1, self.id)
+
+n1 = Nodo(1)
+n2 = Nodo(2)
+
+print(n1)
+print(n2)
---
+++ generadorNodos.py
@@ -0,0 +1,26 @@
+class Nodo:
+    """
+    Clase generadora de Nodos
+    """
+    def __init__(self, id):
+        """
+        Constructor
+        """
+        self.id = id      #Identificador del nodo
+        self.attr = {
+            """
+            Diccionario en construcción
+            """
+        }
+
+    def __str__(self):
+        """
+        Convertir nodo en string
+        """
+        return str(self.id)
+
+    def getNodo(self):
+        """
+        Método para imprimir el identificador del nodo
+        """
+        print(self.id)
